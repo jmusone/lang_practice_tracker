3955520d3836b43fc39bfaa63897b7b8c9fbed75
jmusone <jacobmusone@gmail.com>
2025-03-22 20:42:05 -0400

    Add constants for API call paths

---
+++ src/main/java/com/jng/lang_practice_tracker/constants/Constants.java
@@ -0,0 +1,7 @@
+package com.jng.lang_practice_tracker.constants;
+
+public class Constants {
+    public static final String STUDY_SESSION_PATH = "/studysession";
+    public static final String ID_VARIABLE = "id";
+    public static final String STUDY_SESSION_BY_ID_PATH = STUDY_SESSION_PATH + "/{" + ID_VARIABLE + "}/";
+}
