1c095ce7093597c9e402e101019d79d7d319d89a
jmusone <jacobmusone@gmail.com>
2025-04-14 16:35:51 -0400

    Add updated table from DTO spil

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,6 +1,7 @@
-create table if not exists studySession (
+create table if not exists langStudySession (
        id uuid primary key default gen_random_uuid(),
        description varchar,
+       lang varchar,
        resourceLink varchar,
        resourceMaterial varchar,
        method varchar,
